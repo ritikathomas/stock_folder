199713668a1c795df126f136cc27e021ad8fb1dc
Ritika <ritikathomas.rst@gmail.com>
2026-06-23 12:37:26 +0400

    Add manufacturer contribution visualization

---
+++ view/mfg_contri.py
@@ -0,0 +1,11 @@
+plt.figure(figsize=(12, 10))
+top_n = 10
+other_value = manufacturer_stock_value.iloc[top_n:].sum()
+manufacturer_stock_value_plot = manufacturer_stock_value.iloc[:top_n]
+if other_value > 0:
+    manufacturer_stock_value_plot['Others'] = other_value
+plt.pie(manufacturer_stock_value_plot, labels=manufacturer_stock_value_plot.index, autopct='%1.1f%%', startangle=140, rotatelabels=True, pctdistance=0.85) # Rotate labels and adjust percentage distance
+plt.title(f'Manufacturer Contribution to Total Stock Value (as of {latest_date.strftime("%Y-%m-%d")})', fontsize=14)
+plt.axis('equal')
+plt.tight_layout()
+plt.show()
